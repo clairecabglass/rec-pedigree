1fed511fc9093b19a546b71566237a9f6eab28cf
clairecabglass <289683231+clairecabglass@users.noreply.github.com>
2026-07-18 15:56:12 +0200

    Move training cert name up onto the line

--- app/admin/horses/[id]/training-cert/TrainingCertClient.tsx
+++ app/admin/horses/[id]/training-cert/TrainingCertClient.tsx
@@ -102,7 +102,7 @@ const CertBody = forwardRef<HTMLDivElement, { name: string; templateDataUri: str
         <div
           style={{
             position: "absolute",
-            top: "54.5%",
+            top: "46%",
             left: "8%",
             right: "8%",
             textAlign: "center",
